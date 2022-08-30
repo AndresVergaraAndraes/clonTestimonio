747c89bbdbaa65b4adcc3c54386cc6056cd6d9fe
Andrés <andresvean@outlook.cl>
2022-08-30 08:19:32 -0500

    agregado container de img y texto

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,1 +1,18 @@
 import React from "react";
+
+function Testimonio(){
+return(
+    <div className='testimonio-container'>
+        <img className='imagen-testimonio'
+        src= {require('../imagenes/rust-in-peace.jpg')}
+        alt="Rust in peace"/>
+   <div className="texto-container">
+<p className="nombre-album">Rust in peace</p>
+<p className="nombre-banda">Megadeth</p>
+<p className="descripcion">Rust in Peace es el cuarto álbum 
+de estudio de la banda estadounidense de thrash metal Megadeth, publicado el 24 de septiembre de 1990. Producido por Mike Clink y Dave Mustaine, Rust in Peace marcó el debut del guitarrista Marty Friedman y el baterista Nick Menza, que reemplazaron respectivamente a Jeff Young y Chuck Behler en 1989. Dos sencillos fueron extraídos del álbum, «Hangar 18» y «Holy Wars... The Punishment Due», que son interpretados con regularidad en las actuaciones del grupo.</p>
+   </div>
+    </div>
+)
+}
+export default Testimonio;
